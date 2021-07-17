62b877d141be8725348c21f69e507277908518ee
KingCTer <kingcter@ptd.edu.vn>
2021-07-17 22:38:21 +0700

    Add Aside compoment.

---
+++ HutechStore.Web/Controllers/Components/AsideViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using HutechStore.ApiIntegration;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace HutechStore.Web.Controllers.Components
+{
+    public class AsideViewComponent : ViewComponent
+    {
+        private readonly ICategoryApiClient _categoryApiClient;
+
+        public AsideViewComponent(ICategoryApiClient categoryApiClient)
+        {
+            _categoryApiClient = categoryApiClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var culture = CultureInfo.CurrentCulture.Name;
+
+            var items = await _categoryApiClient.GetAll(culture);
+
+            ViewBag.CurrentCulture = culture;
+            return View(items);
+        }
+    }
+}
--- HutechStore.Web/Startup.cs
+++ HutechStore.Web/Startup.cs
@@ -64,6 +64,22 @@ namespace HutechStore.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                  name: "Product Category Vn",
+                  pattern: "{culture}/danh-muc/{id}", new
+                  {
+                      controller = "Product",
+                      action = "Category"
+                  });
+
+                endpoints.MapControllerRoute(
+                  name: "Product Detail Vn",
+                  pattern: "{culture}/san-pham/{id}", new
+                  {
+                      controller = "Product",
+                      action = "Detail"
+                  });
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
