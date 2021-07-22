a95dfaa9ff7fc6447d07ece11e54ce0c6af93e68
KingCTer <kingcter@ptd.edu.vn>
2021-07-22 09:48:13 +0700

    Add shoping cart

--- HutechStore.Web/wwwroot/assets/js/customization/cart-share.js
+++ HutechStore.Web/wwwroot/assets/js/customization/cart-share.js
@@ -3,12 +3,17 @@
         regsiterEvents();
         loadData();
     }
-    
+
+    this.initializeCart = function () {
+        loadCart();
+    }
+
+    const culture = $('#hidCulture').val();
+
     function regsiterEvents() {
         // Write your JavaScript code.
         $('body').on('click', '.btn-add-cart', function (e) {
             e.preventDefault();
-            const culture = $('#hidCulture').val();
             const id = $(this).data('id');
             $.ajax({
                 type: "POST",
@@ -37,16 +42,17 @@
 
                 $.each(res, function (i, item) {
                     var amount = item.price * item.quantity;
+                    var url = "/" + culture + "/san-pham/" + item.productId;
                     html += '<!--begin::Item-->' +
                         '<div class="d-flex align-items-center justify-content-between p-8">' +
                         '<div class="d-flex flex-column mr-2">' +
-                        '<a href="#" class="font-weight-bold text-dark-75 font-size-lg text-hover-primary">' + item.name + '</a>' +
+                        '<a href="' + url + '" class="font-weight-bold text-dark-75 font-size-lg text-hover-primary">' + item.name + '</a>' +
                         '<span class="text-muted">' + item.description + '</span>' +
                         '<div class="d-flex align-items-center mt-2">' +
                         '<span class="font-weight-bold mr-1 text-dark-75 font-size-lg">' + numberWithCommas(amount) + 'đ</span>' +
                         '<span class="text-muted mr-1">- SL</span>' +
                         '<span class="font-weight-bold mr-2 text-dark-75 font-size-lg">' + item.quantity + '</span>' +
-                        '<a href="#" class="btn btn-xs btn-light-success btn-icon mr-2">' +
+                        '<a href="" class="btn btn-xs btn-light-success btn-icon mr-2">' +
                         '<i class="ki ki-minus icon-xs"></i>' +
                         '</a>' +
                         '<a href="#" class="btn btn-xs btn-light-success btn-icon">' +
@@ -54,7 +60,7 @@
                         '</a>' +
                         '</div>' +
                         '</div>' +
-                        '<a href="#" class="symbol symbol-70 flex-shrink-0">' +
+                        '<a href="' + url + '" class="symbol symbol-70 flex-shrink-0">' +
                         '<img src="' + $('#hidBaseAddress').val() + item.image + '" title="" alt="" />' +
                         '</a>' +
                         '</div>' +
@@ -70,6 +76,50 @@
                 $('#cart_body').html(html);
                 $('#lbl_total').text(numberWithCommas(total) + "đ");
                 $('#lbl_subtotal').text(numberWithCommas(total) + "đ");
+            }
+        });
+    }
+    function loadCart() {
+        $.ajax({
+            type: "GET",
+            url: '/Cart/GetListItems',
+            success: function (res) {
+                var html = '';
+                var total = 0;
+
+                $.each(res, function (i, item) {
+                    var amount = item.price * item.quantity;
+                    var url = "/" + culture + "/san-pham/" + item.productId;
+
+                    html += '<tr>' +
+                        '<td class="d-flex align-items-center font-weight-bolder">' +
+                        '<!--begin::Symbol-->' +
+                        '<div class="symbol symbol-60 flex-shrink-0 mr-4 bg-light">' +
+                        '<div class="symbol-label" style="background-image: url(\'' + $('#hidBaseAddress').val() + item.image + '\')"></div>' +
+                        '</div>' +
+                        '<!--end::Symbol-->' +
+                        '<a href="' + url + '" class="text-dark text-hover-primary">' + item.name + '</a>' +
+                        '</td>' +
+                        '<td class="text-center align-middle">' +
+                        '<a href="javascript:;" class="btn btn-xs btn-light-success btn-icon mr-2">' +
+                        '<i class="ki ki-minus icon-xs"></i>' +
+                        '</a>' +
+                        '<span class="mr-2 font-weight-bolder">' + item.quantity + '</span>' +
+                        '<a href="javascript:;" class="btn btn-xs btn-light-success btn-icon">' +
+                        '<i class="ki ki-plus icon-xs"></i>' +
+                        '</a>' +
+                        '</td>' +
+                        '<td class="text-right align-middle font-weight-bolder font-size-h5">' + numberWithCommas(amount) + 'đ</td>' +
+                        '<td class="text-right align-middle">' +
+                        '<a href="#" class="btn btn-danger font-weight-bolder font-size-sm">Xoá</a>' +
+                        '</td>' +
+                        '</tr>';
+
+                    total += amount;
+                });
+
+                $('#cart_content').html(html);
+                $('#lbl_cart_subtotal').text(numberWithCommas(total) + "đ");
             }
         });
     }
